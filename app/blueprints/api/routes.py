d09b4cb1550fcddb507510824b24e46c3207e9ff
barreraalexander <barreraalexander93@gmail.com>
2022-11-17 12:07:06 -0500

    update to base

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,10 +1,45 @@
 from flask import Blueprint, redirect, url_for, request, jsonify
+import xmltodict
 
 router = Blueprint('api', __name__, url_prefix='/api')
 
+
+# inventory
+# order status
+# ship notices
+# media
+# product data
+# purchase orders
+# product configuration
+# invoices
+
 @router.route('/')
-def home():
-    return jsonify({'here' : 'theresss'})
-# router.route('/api', methods=["GET"])
-# def api_root():
-#     return jsonify({'here' : 'there'})
+def root():
+    return jsonify({'root' : 'root'})
+
+
+# request parameters need to be xml
+# returns need to be xml
+
+@router.route('/inventory')
+def inventory():
+    # print (request)
+    # print (dir(request))
+
+    # print (request.method)
+    request_xml = request.data
+
+
+
+
+    # print (request_xml)
+    xml_dict = xmltodict.parse(request_xml)
+    print (xml_dict)
+
+
+
+
+    return jsonify({'root' : 'root'})
+
+
+                                                                                                                                
