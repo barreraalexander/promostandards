eda523b8edb3e8546379c32f86d8d97ee83fb65e
barreraalexander <barreraalexander93@gmail.com>
2022-11-16 19:13:58 -0500

    update to the base, adding empty routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
-
 from app.config import settings
-
 
 def create_app():
     app = Flask (__name__)
--- app/config.py
+++ app/config.py
@@ -2,9 +2,8 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     debug: bool = False
-    secret_key: str
+    secret_key: str = 'S#perS3crEt_9999'
     server_port: int = 5000
-
 
     class Config:
         env_file = "./.env"
